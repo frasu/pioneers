11fd75082f964b3b168706d41eb925ab7ca43d42
pawel <frasek@gmail.com>
2019-01-25 20:04:04 +0100

    commit with a

--- index.php
+++ index.php
@@ -8,24 +8,33 @@
 
 ?>
 
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <title>Osadnicy - gra przeglądarkowa</title>
-    </head>
-    <body>
-        Tylko tłuści ujrzeli koniec talerza - Spaślak z Chicago<br /><br />
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title>Sejf</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 
-        Nie masz konta? <a href="register.php">Zarejestruj się już teraz!</a><br /><br />
-
+	<div class="menu">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="register.php">Registration</a></li>
+			<li><a href="#">Our Team</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul>
+	</div>
+    <div class="content">
         <?php
-            if (isset($_SESSION['error'])) {
-                echo $_SESSION['error'] . "<br /><br />";
-                unset($_SESSION['error']);
-            }
+        if (isset($_SESSION['error'])) {
+            echo $_SESSION['error'] . "<br /><br />";
+            unset($_SESSION['error']);
+        }
         ?>
-
         <form action="zaloguj.php" method="post">
 
             Login: <br />
